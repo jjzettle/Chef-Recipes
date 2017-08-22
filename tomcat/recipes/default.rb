5df6762fa6f54a2b87706d61bb9cd8e744b421b7
JJ Zettle <31146519+jjzettle@users.noreply.github.com>
2017-08-21 22:36:56 -0700

    Add files via upload

--- tomcat/recipes/default.rb
+++ tomcat/recipes/default.rb
@@ -3,7 +3,7 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
-package 'java-1.8.0-openjdk-devel.x86_64'
+package 'java-1.7.0-openjdk-devel'
 
 group 'chef' do
   action :create
@@ -14,8 +14,8 @@ user 'chef' do
   group 'chef'
 end
 
-remote_file '/tmp/apache-tomcat-8.5.20.tar.gz' do
-  source 'http://mirror.olnevhost.net/pub/apache/tomcat/tomcat-8/v8.5.20/bin/apache-tomcat-8.5.20.tar.gz'
+remote_file '/tmp/apache-tomcat-8.0.33.tar.gz' do
+  source 'http://archive.apache.org/dist/tomcat/tomcat-8/v8.0.33/bin/apache-tomcat-8.0.33.tar.gz'
 end
 
 directory '/opt/tomcat' do
@@ -24,29 +24,21 @@ directory '/opt/tomcat' do
 end
 
 execute 'extract_tomcat' do
-  command 'tar xvf apache-tomcat-8.5.20.tar.gz -C /opt/tomcat --strip-components=1'
+  command 'tar xvf apache-tomcat-8.0.33.tar.gz -C /opt/tomcat --strip-components=1'
   cwd '/tmp'
 end
 
 execute 'chgrp -R chef /opt/tomcat/conf'
 
-execute 'chgrp -R chef /opt/tomcat/bin'
-
-directory '/opt/tomcat' do
-  group 'chef'
-  mode '0755'
-end
-
-directory '/opt/tomcat/conf' do
-  group 'chef'
-  mode '0474'
+execute 'chmod g+rwx conf' do
+  cwd '/opt/tomcat'
 end
 
 execute 'chmod g+r conf/*' do
   cwd '/opt/tomcat'
 end
 
-execute 'chown -R chef webapps/ work/ temp/ logs/ conf/ bin/' do
+execute 'chown -R chef webapps/ work/ temp/ logs/ conf/' do
   cwd '/opt/tomcat'
 end
 
